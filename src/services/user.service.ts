2521307b835b6afa3a64a7846f1ac79266812ba0
Muhammad <90960385+muhammad-abs@users.noreply.github.com>
2026-05-17 17:09:08 +0300

    tasks completed

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,10 +1,11 @@
 import { inject, Injectable } from '@angular/core';
-import { BehaviorSubject, catchError, finalize, Observable, of, tap } from 'rxjs';
+import { BehaviorSubject, catchError, delay, filter, finalize, Observable, of, tap } from 'rxjs';
 import { IUser } from '../interfaces/IUser';
 import { UserApiService } from './user-api.service';
 import { LoaderService } from './loader.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MessageService } from './message.service';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
   providedIn: 'root',
@@ -14,29 +15,55 @@ export class UserService {
   private userApiService: UserApiService = inject(UserApiService);
   private loaderService: LoaderService = inject(LoaderService);
   private messageService: MessageService = inject(MessageService);
+  private localStorageService: LocalStorageService = inject(LocalStorageService);
   
   private usersSubject: BehaviorSubject<IUser[]> = new BehaviorSubject<IUser[]>([]);
   users$: Observable<IUser[]> = this.usersSubject.asObservable();
   
-  setUsers(user: IUser[]): void {
-    this.usersSubject.next(user);
+  private searchSubject : BehaviorSubject<string> = new BehaviorSubject<string>('');
+  search$: Observable<string> = this.searchSubject.asObservable();
+
+  private USERS_KEY: string = 'users';
+  
+  setUsers(users: IUser[]): void {
+    this.usersSubject.next(users);
+  }
+  
+  handleCreateUser(user: IUser): void {
+    this.updateUsers(this.getUsers().concat(user));
   }
   
   getUsers(): IUser[] {
     return this.usersSubject.getValue();
   }
   
-  loadUsers(): Observable<IUser[]> {
-    this.loaderService.showLoader();
-    
-    return this.userApiService.getUsers()
-      .pipe(
-        catchError((error: HttpErrorResponse) => {
-          this.messageService.showError('Пользователи не были загружены');
-          return of([]);
-        }),
-        finalize(() => this.loaderService.hideLoader())
-      );
+  handleDeleteUser(id: number): void {
+    this.updateUsers(this.getUsers().filter((currentUser: IUser) => currentUser.id !== id));
   }
   
+  updateUsers(updatedUsers: IUser[]): void {
+    this.setUsers(updatedUsers);
+    this.localStorageService.setValue(this.USERS_KEY, updatedUsers);
+  }
+  
+  onSearchChanged(search: string): void {
+    this.searchSubject.next(search);
+  }
+  
+  loadUsers(): Observable<IUser[]> {
+    const savedUsers: IUser[] | null = this.localStorageService.getValue<IUser[]>(this.USERS_KEY);
+    if (savedUsers) {
+      return of(savedUsers);
+    } else {
+      this.loaderService.showLoader();
+      return this.userApiService.getUsers()
+        .pipe(
+          finalize(() => this.loaderService.hideLoader()),
+          catchError((error: HttpErrorResponse) => {
+            this.messageService.showError('Пользователи не были загружены');
+            return of([]);
+          }),
+        );
+    }
+  }
 }
--- src/users-page/users-page.component.ts
+++ src/users-page/users-page.component.ts
@@ -2,11 +2,15 @@ import { Component, inject } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { AsyncPipe } from '@angular/common';
 import { IUser } from '../interfaces/IUser';
-import { Observable, tap } from 'rxjs';
+import { combineLatest, map, Observable, pipe } from 'rxjs';
+import { UserCardComponent } from "../user-card/user-card.component";
+import { CreateUserComponent } from '../create-user/create-user.component';
+import { UsersFilterComponent } from '../users-filter/users-filter.component';
+
 
 @Component({
   selector: 'app-users-page',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, UserCardComponent, CreateUserComponent, UsersFilterComponent],
   templateUrl: './users-page.component.html',
   styleUrl: './users-page.component.scss',
 })
@@ -14,9 +18,34 @@ export class UsersPageComponent {
   
   private userService: UserService = inject(UserService);
   
-  users$: Observable<IUser[]> = this.userService.loadUsers()
-    .pipe(
-      tap((users: IUser[]) => this.userService.setUsers(users))
-    );
-    
+  users$: Observable<IUser[]> = this.userService.users$;
+  search$: Observable<string> = this.userService.search$;
+
+  ngOnInit(): void {
+    this.userService.loadUsers().pipe(
+      map((users: IUser[]) => {
+        this.userService.setUsers(users);
+      })
+    ).subscribe();
+  }
+  
+  handleDeleteUser($event: number): void {
+    this.userService.handleDeleteUser($event);
+  }
+  
+  handleCreateUser(user: IUser): void {
+    this.userService.handleCreateUser(user);
+  }
+  
+  onSearchChanged(search: string): void {
+    this.userService.onSearchChanged(search);
+  }
+  
+  filteredUsers$: Observable<IUser[]> = combineLatest([this.users$,this.search$]).pipe(
+    map(([users, search]: [IUser[], string]) => {
+      const normalizedSearch: string = search.toLowerCase().trim();
+      return users.filter((user: IUser) => user.name.toLowerCase().trim().includes(normalizedSearch));
+    })
+  );
+  
 }
